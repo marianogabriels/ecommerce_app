36905df0a7a8166ca771039c5f92d43f9ee8184a
Mariano Schmidt <marianogabriels@gmail.com>
2013-07-24 00:00:34 -0300

    depot scaffold and bootstrap

---
+++ Rakefile
@@ -0,0 +1,16 @@
+#!/usr/bin/env rake
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+
+Depot::Application.load_tasks
+
+namespace :db do
+  task :regenerate do
+   system("rake db:drop RAILS_ENV=development")
+   system("rake db:migrate")
+  end
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ac4ca75850de91ad25a95af17eed02bf3c082a217a3abb263a6046f8a805decdda156101693b4494a63854ad3d1883954bc11af64fec269b7833f13b99859a28'
